f547a12f31f5b50c0f834af5b62c4c3b7eabb776
Daniil <94884910+Filienko@users.noreply.github.com>
2024-07-08 12:26:01 -0700

    Changing views

--- ml_services/api/views.py
+++ ml_services/api/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify, current_app
 from ml_services.api.ml_utils import predict_score
+import os
 
 # Create a Blueprint
 base_blueprint = Blueprint('base', __name__, cli_group=None)
@@ -16,12 +17,14 @@ def predict_score_route():
     # model_path = data.get('model_path')
     model_path = current_app.config.get('TORCH_MODEL_PATH')
 
-    if not message or not model_path:
+    if not message:
         return jsonify({'error': 'Invalid input'}), 400
 
     try:
-        # score = predict_score(message, model_path)
-        score = 'success'
+        if not model_path or not os.path.exists(model_path):
+            score = 'success'
+        else:
+            score = predict_score(message, model_path)
         return jsonify({'score': score}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
